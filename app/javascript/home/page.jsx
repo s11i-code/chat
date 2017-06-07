0afd1cf0f1dea4c13220686433aa5a32172c0d04
Satu <satu@Satus-MacBook-Pro.local>
2017-06-07 19:03:33 +0300

    Add fonts

--- app/javascript/home/page.jsx
+++ app/javascript/home/page.jsx
@@ -4,6 +4,9 @@ export default React.createClass({
 
   render() {
     return (
-      <div className='container'><h1>This is the home page</h1></div>);
+      <div className='container'>
+        <h1>Chat app</h1>
+        <p>LOkgermgkemglkdmdlmk</p>
+    </div>);
   },
 });
