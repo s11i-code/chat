e64e05ffbec9796eceb2b54a9795d96a2c342d1d
Satu <satu@Satus-MacBook-Pro.local>
2017-06-07 18:36:58 +0300

    Add linting

--- app/javascript/home/page.jsx
+++ app/javascript/home/page.jsx
@@ -1,12 +1,9 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
+import React from 'react';
 
-import React from 'react'
 export default React.createClass({
 
-    render(){
-        return (
-    <div className='container'><h1>This is the home page</h1></div>)
-  }
-})
+  render() {
+    return (
+      <div className='container'><h1>This is the home page</h1></div>);
+  },
+});
--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -1,13 +1,10 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from '../home/page';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Page from '../home/page'
-
+/* global document */
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render( <Page/>,
+  ReactDOM.render(<Page />,
     document.body.appendChild(document.createElement('div')),
-  )
-})
+  );
+});
